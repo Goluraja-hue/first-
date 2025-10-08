72a982217d5fb81a56c9a4ef3765294534957152
Goluraja-hue <kumargolu934113@gmail.com>
2025-10-08 14:42:36 +0530

    complete

---
+++ js_project/fork/main.js
@@ -0,0 +1,23 @@
+const api = 'https://dummyjson.com/quotes/random';
+// const api = "https://quotes.domiadi.com/api"
+
+let quote = document.querySelector('.quote');
+let author = document.querySelector('.author');
+let btn = document.querySelector('button');
+btn.addEventListener("click",getquote)
+
+
+function getquote() {fetch(api)
+.then((data) => {
+    return data.json();
+})
+.then((res) => {
+    console.log(res);
+quote.textContent = ` ‶ ${res.quote} ‶`;
+author.textContent = ` — ${res.author}`;
+})
+.catch((error) => {
+    console.log(error);
+});
+}
+
